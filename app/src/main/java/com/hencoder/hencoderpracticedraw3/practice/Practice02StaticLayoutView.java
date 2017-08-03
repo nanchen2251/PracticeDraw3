8c3244b651f552f3157c0462ae48b2c6144cec7b
刘世麟 <503233512@qq.com>
2017-08-03 16:38:41 +0800

    除开最后两个的更新

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice02StaticLayoutView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice02StaticLayoutView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
+import android.text.Layout.Alignment;
+import android.text.StaticLayout;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
@@ -28,6 +30,8 @@ public class Practice02StaticLayoutView extends View {
         textPaint.setTextSize(60);
     }
 
+    StaticLayout mStaticLayout = new StaticLayout(text,textPaint,600, Alignment.ALIGN_NORMAL,1,0,true);
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -35,5 +39,6 @@ public class Practice02StaticLayoutView extends View {
         // 使用 StaticLayout 代替 Canvas.drawText() 来绘制文字，
         // 以绘制出带有换行的文字
         canvas.drawText(text, 50, 100, textPaint);
+        mStaticLayout.draw(canvas);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice12MeasureTextView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice12MeasureTextView.java
@@ -38,9 +38,10 @@ public class Practice12MeasureTextView extends View {
         super.onDraw(canvas);
 
         // 使用 Paint.measureText 测量出文字宽度，让文字可以相邻绘制
-
         canvas.drawText(text1, 50, 200, paint1);
-        canvas.drawText(text2, 50 + 100, 200, paint2);
-        canvas.drawText(text3, 50 + 200, 200, paint1);
+        float spacing1 = paint1.measureText(text1);
+        canvas.drawText(text2, 50 + spacing1, 200, paint2);
+        float spacing2 = paint2.measureText(text2);
+        canvas.drawText(text3, 50 + spacing1 + spacing2, 200, paint1);
     }
 }
